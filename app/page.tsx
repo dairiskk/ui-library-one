bf4a85062735dc52c541420478acbb0c5c9b446f
dairiskk <dairis.klesniks@gmail.com>
2025-02-25 18:24:30 +0200

    dawda

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,11 @@
 'use client'
-import { Button } from "@/components/Button";
-import { Card } from "@/components/Card";
-import { Dropdown } from "@/components/Dropdown";
-import { Input } from "@/components/Input";
-import { Modal } from "@/components/Modal";
-import { Spinner } from "@/components/Spinner";
-import { ToggleSwitch } from "@/components/ToggleSwitch";
-import Image from "next/image";
 import React from "react";
-import { ChangeEvent } from "react";
 
 export default function Home() {
-  const [likes, setLikes] = React.useState(0);
-  function setIsModalOpen(arg0: boolean) {
-    throw new Error("Function not implemented.");
-  }
-
-  function setTrue(event: ChangeEvent<HTMLInputElement>): void {
-    throw new Error("Function not implemented.");
-  }
 
   return (
     <div >
-        <Button text="Click Me" className="mb-4" onClick={() => setIsModalOpen(true)} />
-          <Input label="Password" placeholder = "PEEEE" ></Input>
-          <Card title = "dawdwa" description = "desc adawd da" buttonText="SUBMIT" children = {<Button text="dadwad"></Button>}></Card>
-          <ToggleSwitch checked={true} onChange={setTrue}></ToggleSwitch>
-          <Dropdown label = "dropdown1" options = {[{ value: '1', label: 'test1' }, { value: '2', label: 'test2' }]}></Dropdown>
-          <Spinner></Spinner>
-          <Modal isOpen={true} onClose={function (): void {
-        throw new Error("Function not implemented.");
-      } } title={"test 1234"} children={undefined}></Modal>
+
     </div>
   );
 }
--- components/Spinner.tsx
+++
@@ -1,5 +0,0 @@
-export function Spinner({ className = "" }) {
-    return (
-      <div className={`animate-spin rounded-full h-6 w-6 border-b-2 border-black ${className}`}></div>
-    );
-  }
